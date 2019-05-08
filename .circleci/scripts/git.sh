5e52d22f5934d1d1700660635f386f2318d3eca2
Stephen Hallett <stephen.hallett@catch.com.au>
2019-05-09 08:29:16 +1000

    deploy to testing on tag as well

--- .circleci/scripts/git.sh
+++ .circleci/scripts/git.sh
@@ -1,54 +1,59 @@
 #!/bin/bash
 set -e -x
-[[ -n "$CIRCLE_TAG" ]] && export ENV_TAG=production || export ENV_TAG=test
-[[ ${ENV_TAG} == "production" ]] && export BRANCH=ops || export BRANCH=rbt
-[[ ${ENV_TAG} == "production" ]] && export CHART=stable || export CHART=testing
 
 export DOCKER_TAG=${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:12}}
 export PR_BRANCH_NAME=${BRANCH}-${CIRCLE_PROJECT_REPONAME}-${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:12}}
 
+function update_veritas() {
 
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/cgws/veritas -b $BRANCH
-cd veritas
-git pull
+  [[ ${ENV_TAG} == "production" ]] && export BRANCH=ops || export BRANCH=rbt
+  [[ ${ENV_TAG} == "production" ]] && export CHART=stable || export CHART=testing
 
-git checkout -b $PR_BRANCH_NAME
+  git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/cgws/veritas -b $BRANCH
+  cd veritas
+  git pull
 
-###### Check for a project_namespace env
-[ -n "${PROJECT_NAMESPACE}" ] || export PROJECT_NAMESPACE=$CIRCLE_PROJECT_REPONAME
+  git checkout -b $PR_BRANCH_NAME
 
-########### MANIFEST
-#cat releases/$CIRCLE_PROJECT_REPONAME.yaml | yq --arg projname "$CIRCLE_PROJECT_REPONAME" --arg newversion "v1.0.1" '.spec.template.spec.containers |= map(select(.name == $projname).image = "362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/$projname:$newversion" )'
+  ###### Check for a project_namespace env
+  [ -n "${PROJECT_NAMESPACE}" ] || export PROJECT_NAMESPACE=$CIRCLE_PROJECT_REPONAME
 
-if [ ! -f namespaces/$PROJECT_NAMESPACE.yaml ]; then
+  ########### MANIFEST
+
+  if [ ! -f namespaces/$PROJECT_NAMESPACE.yaml ]; then
+      echo "File not found!"
+      envsubst < ~/code/.circleci/templates/namespace.yaml | sponge  namespaces/$PROJECT_NAMESPACE.yaml
+  fi
+
+
+  ########## HELM Values
+  if [ ! -f releases/$CIRCLE_PROJECT_REPONAME.yaml ]; then
     echo "File not found!"
-    envsubst < ~/code/.circleci/templates/namespace.yaml | sponge  namespaces/$PROJECT_NAMESPACE.yaml
-fi
+    envsubst < ~/code/.circleci/templates/release.yaml | sponge  releases/$CIRCLE_PROJECT_REPONAME.yaml
+  else
+      cat releases/$CIRCLE_PROJECT_REPONAME.yaml | yq -y --arg projname "$CIRCLE_PROJECT_REPONAME" --arg newversion "${DOCKER_TAG}" '.spec.values.image.tag = $newversion' | sponge releases/$CIRCLE_PROJECT_REPONAME.yaml
+  fi
 
+  git config --global user.email "circleci@catch.com.au"
+  git config --global user.name "Circle"
 
-########## HELM Values
-if [ ! -f releases/$CIRCLE_PROJECT_REPONAME.yaml ]; then
-  echo "File not found!"
-  envsubst < ~/code/.circleci/templates/release.yaml | sponge  releases/$CIRCLE_PROJECT_REPONAME.yaml
-else
-    cat releases/$CIRCLE_PROJECT_REPONAME.yaml | yq -y --arg projname "$CIRCLE_PROJECT_REPONAME" --arg newversion "${DOCKER_TAG}" '.spec.values.image.tag = $newversion' | sponge releases/$CIRCLE_PROJECT_REPONAME.yaml
-fi
+  git add releases/$CIRCLE_PROJECT_REPONAME.yaml
+  git add namespaces/$PROJECT_NAMESPACE.yaml
 
-git config --global user.email "circleci@catch.com.au"
-git config --global user.name "Circle"
+  git commit -m "[$BRANCH] UPDATE $CIRCLE_PROJECT_REPONAME image to $DOCKER_TAG"
 
-git add releases/$CIRCLE_PROJECT_REPONAME.yaml
-git add namespaces/$PROJECT_NAMESPACE.yaml
+  if [ "$ENV_TAG" == "production" ]; then
+    git push origin $PR_BRANCH_NAME
+    ls -lah /usr/local/bin
+    mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+    /tmp/hub pull-request -b $BRANCH -m "$(git log -1 --pretty=%B)"
+  else
+      git checkout $BRANCH
+      git merge --ff-only $PR_BRANCH_NAME
+      git push
+  fi
 
-git commit -m "[$BRANCH] UPDATE $CIRCLE_PROJECT_REPONAME image to $DOCKER_TAG"
+}
 
-if [ "$ENV_TAG" == "production" ]; then
-  git push origin $PR_BRANCH_NAME
-  ls -lah /usr/local/bin
-  mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
-  /tmp/hub pull-request -b $BRANCH -m "$(git log -1 --pretty=%B)"
-else
-    git checkout $BRANCH
-    git merge --ff-only $PR_BRANCH_NAME
-    git push
-fi
+[[ -n "$CIRCLE_TAG" ]] && ENV_TAG=production update_veritas
+ENV_TAG=test update_veritas
--- .circleci/scripts/helm_chart_repo_update.sh
+++ .circleci/scripts/helm_chart_repo_update.sh
@@ -3,47 +3,37 @@ set -e -x
 export DOCKER_TAG=${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:12}}
 export PR_BRANCH_NAME=${CIRCLE_PROJECT_REPONAME}-${DOCKER_TAG}}
 
-[ -n "${CIRCLE_TAG}" ] && export ENV_TAG=stable || export ENV_TAG=testing
+function update_helm_charts() {
+    apk add bash
+    helm init --client-only && \
+      helm plugin install https://github.com/hypnoglow/helm-s3.git && \
+      helm repo add cgws-helm-stable s3://cgws-helm/stable && \
+      helm repo add cgws-helm-testing s3://cgws-helm/testing
 
-apk add bash
-helm init --client-only && \
-  helm plugin install https://github.com/hypnoglow/helm-s3.git && \
-  helm repo add cgws-helm-stable s3://cgws-helm/stable && \
-  helm repo add cgws-helm-testing s3://cgws-helm/testing
+    git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/cgws/helm-charts
+    cd helm-charts/charts
 
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/cgws/helm-charts
-cd helm-charts/charts
-# if [ "x${ENV_TAG}" == "xstable" ]; then
-#     git checkout -b $PR_BRANCH_NAME
-# fi
+    helm repo update
 
-helm repo update
+    cd $ENV_TAG && helm fetch cgws-helm-$ENV_TAG/$CIRCLE_PROJECT_REPONAME --untar --devel
 
-cd $ENV_TAG && helm fetch cgws-helm-$ENV_TAG/$CIRCLE_PROJECT_REPONAME --untar --devel
-# [ -n "${CIRCLE_TAG}" ] && mv $CIRCLE_PROJECT_REPONAME ${CIRCLE_PROJECT_REPONAME}-${CIRCLE_TAG}
+    if [ -n "$(git status --porcelain)" ]; then
+       echo "Changes in chart detected proceeding"
+    else
+        echo "No chart changes, exiting!"
+        exit 0
+    fi
 
-if [ -n "$(git status --porcelain)" ]; then
-   echo "Changes in chart detected proceeding"
-else
-    echo "No chart changes, exiting!"
-    exit 0
-fi
+    git config --global user.email "circleci@catch.com.au"
+    git config --global user.name "Circle"
 
-git config --global user.email "circleci@catch.com.au"
-git config --global user.name "Circle"
+    git add -A
+    git commit -a -m "[${ENV_TAG}] UPDATE/ADD Chart: ${CIRCLE_PROJECT_REPONAME}-${DOCKER_TAG}"
 
-git add -A
-git commit -a -m "[${ENV_TAG}] UPDATE/ADD Chart: ${CIRCLE_PROJECT_REPONAME}-${DOCKER_TAG}"
+    git push
 
-# if [ "x${ENV_TAG}" == "xstable" ]; then
-#     git push origin $PR_BRANCH_NAME
-# fi
-git push
-# ls -lah /usr/local/bin
+}
 
+[ -n "${CIRCLE_TAG}" ] && ENV_TAG=stable update_helm_charts
 
-# mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
-
-# if [ "x${ENV_TAG}" == "xstable" ]; then
-#     /tmp/hub pull-request -b master -m "$(git log -1 --pretty=%B)"
-# fi
+ENV_TAG=testing update_helm_charts
